4cd372e3d91baeb7a6c2807f89b8976083647a54
Thiago de Souza Crepequer <thiago.crepequer@hotmail.com>
2023-02-06 11:54:34 -0300

    aprimorado consideravelmente o sistema de pesquisa

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,60 +2,44 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
-function Home(props) {
+function Home() {
     const [dataCompleto, setDataCompleto] = useState([])
-    const [data, setData] = useState([])
-    const [selecionado, setSelecionado] = useState()
+    const [selecionado, setSelecionado] = useState('')
     const [pesquisa, setPesquisa] = useState('')
 
     useEffect(() => {
         axios.get('https://pokedex-server.onrender.com/pokemon').then(response => {
             let entries = Object.entries(response.data)
             setDataCompleto(entries)
-            setData(entries);
         })
       }, [])
 
-    function PesquisaPokemon() {
-        return (
+    // Lógica da pesquisa filtrando os dados de dataCompleto
+    let filtrado = dataCompleto.filter(item => item[0].startsWith(pesquisa.toLowerCase()))
+    filtrado = filtrado.filter(item => item[1].type.some(type => type.type.name.startsWith(selecionado)))
+
+    return (
+        <>
             <form className='formulario-pesquisa'>
-                <input type="test" id='input-pokemon' placeholder='Pesquise um pokemon'/>
+                <input 
+                    type="search" 
+                    id='input-pokemon' 
+                    placeholder='Pesquise um pokemon'
+                    value={pesquisa}
+                    onChange={(event) => {
+                        setPesquisa(event.target.value)
+                    }}
+                    />
                 <input type='submit' className='botao-pesquisar'/>
             </form>
-        )
-    }
-
-    useEffect(() => {
-        // usar o props.onChange no input tava causando problema com a digitação
-        const input_pokemon = document.getElementById('input-pokemon')
-        input_pokemon.addEventListener('blur', textChange)
-        input_pokemon.addEventListener('keypress', function(res) {
-            if(res.key === 'Enter') {
-                textChange()
-            }
-        })
-    })
-
-    useEffect(() => {
-        if(pesquisa === '') {
-            setData(dataCompleto)
-        } else {
-            setData(dataCompleto.filter(item => item[0].startsWith(pesquisa.toLowerCase())).map(item => item))
-        }
-    }, [pesquisa])
-
-    const textChange = (event) => {
-        if(!event) {
-            return setPesquisa('')
-        } 
-        setPesquisa(event.target.value)
-    }
-
-    function CaixaSelecao(props) {
-        return (
-            // Era mais facil usar o map para escrever todos
-            <select value={props.value} onChange={props.onChange} className="select-type">
-                <option value='inicio'>Início</option>
+            <select 
+                className="select-type"
+                value={selecionado} 
+                onChange={(event) => {
+                    setSelecionado(event.target.value);
+                }} 
+            >
+                <option value=''>Início</option>
                 <option value='fire'>Fire</option>
                 <option value='grass'>Grass</option>
                 <option value='poison'>Poison</option>
@@ -73,42 +57,14 @@ function Home(props) {
                 <option value='flying'>Flying</option>
                 <option value='psychic'>Psychic</option>
             </select>
-        )
-    }
-
-    const handleChance = (event) => {
-        setSelecionado(event.target.value)
-    }
-
-    useEffect(() => {
-        if(selecionado === 'inicio') {
-            setData(dataCompleto)
-        } else {
-            setData(dataCompleto.filter(element => {
-                return element[1].type.some(type => type.type.name === selecionado) 
-            }))
-        }
-    }, [selecionado])
-        
-    function RepetBlocos(props) {
-        return (
             <div className='pokemons-container'>
-                {props.value.map(element => (
+                {filtrado.map(element => (
                     <div key={element[0]} className="bloco-pokemon">
-                        <BlocoPokemon value={element} /> 
-                        
+                        <BlocoPokemon value={element} />  
                     </div>
                 ))}
             </div> 
-        )
-    }
-
-    return (
-        <div>
-            <PesquisaPokemon/>
-            <CaixaSelecao value={selecionado} onChange={handleChance}/>
-            <RepetBlocos value={data}/>  
-        </div>
+        </>
   
     );
 }
@@ -123,17 +79,11 @@ function BlocoPokemon(props) {
             {Object.entries(element[1].type).map((type) => (
                <img src={`../../${type[1].type.name}.jpg`} width='34px' alt={type[1].type.name} key={type[1].type.name}></img>
           ))}
-            <Button to={`/pokemon/${element[1].id}`}>Saiba mais</Button>
+            <Link to={`/pokemon/${element[1].id}`}>
+                <button className='botao'>Saiba mais</button> 
+            </Link>
         </div>
     )
 }
-
-function Button({ to, children }) {
-    return(
-      <Link to={to}>
-        <button  className='botao'>{children}</button> 
-      </Link>
-    )
-} 
 
 export default Home
